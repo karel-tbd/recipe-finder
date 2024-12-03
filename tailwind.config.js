d7c910b9872ea92560eae9aa7e0989389dc5b3dd
Karel Scheers <karel@tbd.be>
2024-12-03 18:37:49 +0100

    installation packages

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+    "./src/Twig/Components/**/*.php",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      },
+    },
+
+  },
+  plugins: [],
+}
