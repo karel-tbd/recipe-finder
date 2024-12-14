62d7eaa21e83095f300140986be35af28869232a
Karel Scheers <karel@tbd.be>
2024-12-14 12:25:42 +0100

    Made all igridient is db with foodgroup

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\FoodGroupRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/home', name: 'app_home')]
+    public function index(FoodGroupRepository $foodGroupRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $foodGroups = $foodGroupRepository->findAll();
+        return $this->render('home/index.html.twig',[
+            'foodGroups' => $foodGroups,
+        ]);
     }
 }
