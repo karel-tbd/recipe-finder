b219d98fdd5997f3b81e1b989ce164fe53842812
Karel Scheers <karel@tbd.be>
2024-12-19 19:10:52 +0100

    made search live and added dish type in search

---
+++ src/Twig/Components/Accordion/Item.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Twig\Components\Accordion;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class Item
+{
+}
---
+++ src/Twig/Components/Pages/Search.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Twig\Components\Pages;
+
+use App\Form\SearchFormType;
+use App\Repository\RecipeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent]
+final class Search extends AbstractController
+{
+    use DefaultActionTrait;
+    use ComponentWithFormTrait;
+
+    #[LiveProp(writable: true)]
+    public string $query = '';
+
+    public function __construct(private readonly RecipeRepository $recipeRepository)
+    {
+    }
+
+    public function getRecipes(): array
+    {
+        $search = $this->getForm()->getData() ?? [];
+        $recipes = $this->recipeRepository->search($search);
+        return $recipes;
+    }
+
+    protected function instantiateForm(): FormInterface
+    {
+        return $this->createForm(SearchFormType::class);
+    }
+
+    private function getDataModelValue(): ?string
+    {
+        return 'on(input)|*';
+    }
+}
