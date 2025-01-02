8e60746ae862662244bafbb4b08003b1c50e347c
Karel Scheers <karel@tbd.be>
2025-01-02 16:47:32 +0100

    amount on show is fixible

---
+++ src/Twig/Components/Pages/ShowRecipe.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Twig\Components\Pages;
+
+use App\Entity\Recipe;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\TwigComponent\Attribute\PostMount;
+
+#[AsLiveComponent]
+final class ShowRecipe
+{
+    use DefaultActionTrait;
+
+    #[LiveProp]
+    public Recipe $recipe;
+
+    #[LiveProp]
+    public int $people;
+
+    #[LiveProp]
+    public int $score;
+
+    #[PostMount]
+    public function postMount(): void
+    {
+        $this->people = $this->recipe->getPeople();
+    }
+
+    #[LiveAction]
+    public function updatePeople(#[LiveArg] int $people)
+    {
+        $this->people = $people;
+    }
+
+}
